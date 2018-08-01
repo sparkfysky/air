e1ee41a5b92077cade94f186d78fbd88fe2663a0
sparkfysky <33694228+sparkfysky@users.noreply.github.com>
2018-08-01 15:28:22 +0800

    判断三角形阶乘排列组合

--- main.cpp
+++ main.cpp
@@ -1,94 +1,53 @@
 #include <iostream>
-#include <string.h>
+#include <math.h>
 using namespace std;
-#define maxlenth 100
-#include <vector>
 
-        int maxlen1(string a,string b){                //只返回长度
-                char *a1=new char[maxlenth];
-                char *b1=new char[maxlenth];
-                strcpy(a1,a.c_str());
-                strcpy(b1,b.c_str());
-                int tempi=0,tempj=0;
-                int templenth=0;
-                int maxlen=0;
+float triangle(float a[][3]){                      //判断三角形是否在三维空间存在，存在计算面积，不存在返回0
+        //a坐标a[0][1]a[0][2]a[0][3]
+        float lenth1=sqrt(pow((a[0][0]-a[1][0]),2)+pow((a[0][1]-a[1][1]),2)+pow((a[0][2]-a[1][2]),2));
+        float lenth2=sqrt(pow((a[0][0]-a[2][0]),2)+pow((a[0][1]-a[2][1]),2)+pow((a[0][2]-a[2][2]),2));
+        float lenth3=sqrt(pow((a[1][0]-a[2][0]),2)+pow((a[1][1]-a[2][1]),2)+pow((a[1][2]-a[2][2]),2));
+        if (lenth1+lenth2<lenth3||lenth3+lenth2<lenth1||lenth1+lenth3<lenth2)
+                return 0;
+        float p=lenth1+lenth2+lenth3;
+        p=p/2;
+        float sum=sqrt(p*(p-lenth1)*(p-lenth3)*(p-lenth2));
+        return sum;
+}
 
-                for(int i=0;a1[i]!='\0';i++)
-                        for(int j=0;b1[j]!='\0';j++){
-                                if(a1[i]==b1[j]){
-                                        tempi=i;
-                                        tempj=j;
-                                        while(a1[i]==b1[j]&&a[i]!='\0'){
-                                                i++;
-                                                j++;
-                                                templenth++;
-                                        }
-                                        if(templenth>maxlen){
-                                                maxlen=templenth;
-
-                                        }
-                                        templenth=0;
-                                        i=tempi;
-                                        j=tempj;
+        int zuhe(int a,int b){                                  //排列组合，a小b大
+                if(a==0)
+                        return 1;
+                else{
+                        int tempb=1;
+                        int tempa=1;
+                                for(int i=0;i<a;i++){
+                                        tempb=tempb*(b-i);
+                                        tempa=tempa*(a-i);
                                 }
+                        return tempb/tempa;
                 }
-                return maxlen;
-        }
-        int firstOf_a=-1;                               //不需要索引去掉即可
-        int firstOf_b=-1;
-        vector <char> result_array;
-        int maxlen2(string a,string b){                   //返回长度，重复子序列放在vector中，最开始重复字母索引放在全局变量firstOf_a之中
-                char *a1=new char[maxlenth];
-                char *b1=new char[maxlenth];
-                strcpy(a1,a.c_str());
-                strcpy(b1,b.c_str());
-                int tempi=0,tempj=0;
-                int templenth=0;
-                int maxlen=0;
-                vector <char>temp;
-                for(int i=0;a1[i]!='\0';i++)
-                        for(int j=0;b1[j]!='\0';j++){
-                                if(a1[i]==b1[j]){
-                                        tempi=i;
-                                        tempj=j;
-                                        while(a1[i]==b1[j]&&a[i]!='\0'){
-                                                temp.push_back(a[i]);
-                                                i++;
-                                                j++;
-                                                templenth++;
 
-                                        }
-                                        if(templenth>maxlen){
-                                                maxlen=templenth;
-                                                firstOf_a=i-templenth;                  //不需要索引去掉即可
-                                                firstOf_b=j-templenth;                  //
-                                                result_array=temp;
-
-                                        }
-                                        templenth=0;
-                                        temp.clear();
-                                        i=tempi;
-                                        j=tempj;
-                                }
-                }
-                return maxlen;
         }
 
+        int cal(int a,int b, int c,int d){                    //C(a,b)*C(c,d)
+                int temp=1;
+                temp=zuhe(a,b)*zuhe(c,d);
+                return temp;
+        }
 
-//3-2
-//len(a)-len(max(相同子列2))
+        int jiecheng(int a){                                 //阶乘
+                if(a==0)
+                        return 1;
+                long long temp=a;
+                for(int i=a-1;i>0;i--)
+                        temp=temp*i;
+                return temp;
+        }
 int main()
 {
-        string a;
-        string b;
-        cin>>a>>b;
-
-        //int result=maxlen1(a,b);
-        int result=maxlen2(a,b);
-
-        cout<<result<<endl<<"showoff"<<endl;
-        for(vector <char>::iterator it=result_array.begin();it!=result_array.end();it++)
-                cout<<*it;
-        cout<<endl<<"Index a"<<firstOf_a<<endl<<"Index b"<<firstOf_b<<endl;
+        float a[3][3]={{0,0,0},{0,1,0},{1,0,0}};
+        cout << triangle(a)<<endl;
+        cout<<jiecheng(0);
         return 0;
 }
